f86fd321013301264b605b42b2351c48425f14a4
Papon <papon.s@lawyerplusgroup.com>
2023-07-23 13:53:59 +0700

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Papon Supamongkonchai on 23/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocado/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Avocado
+//
+//  Created by Papon Supamongkonchai on 23/7/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
